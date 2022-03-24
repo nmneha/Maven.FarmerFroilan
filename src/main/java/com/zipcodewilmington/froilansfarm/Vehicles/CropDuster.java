53bd2c204d682bd8f41a58532b10ccbe85f6a667
Mike <29170001+mike-prentice@users.noreply.github.com>
2022-03-23 22:41:41 -0400

    bug fixes

--- src/main/java/com/zipcodewilmington/froilansfarm/Vehicles/CropDuster.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Vehicles/CropDuster.java
@@ -3,8 +3,27 @@ package com.zipcodewilmington.froilansfarm.Vehicles;
 import com.zipcodewilmington.froilansfarm.Interfaces.FarmVehicle;
 
 public class CropDuster extends Aircraft implements FarmVehicle {
-    public boolean fertilize(){
+    public boolean fertilize() {
         return false;
     }
 
+    @Override
+    public boolean operate() {
+        // TODO Auto-generated method stub
+        return FarmVehicle.super.operate();
+    }
+
+    @Override
+    public boolean fly() {
+
+    }
+
+    @Override
+    public String makeNoise() {
+        // TODO Auto-generated method stub
+        return super.makeNoise();
+    }
+
 }
+
+// needs to "fertilize" a "cropRow"
