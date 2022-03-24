e92dc88a5aa54e4d636fe6ed0cca40eb44b285c0
nusera <nusera.neha@gmail.com>
2022-03-24 14:50:02 -0400

    small changes

--- src/main/java/com/zipcodewilmington/froilansfarm/Interfaces/Produce.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Interfaces/Produce.java
@@ -4,4 +4,11 @@ public interface Produce {
     default Edible yield (){
         return null;
     }
+
+    default boolean hasBeenFertilized(){
+        return true;
+    }
+    default boolean hasBeenHarvested(){
+        return true;
+    }
 }
