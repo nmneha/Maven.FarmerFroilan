b04f7dd40450288a2f1a833245cfc99820efbc2d
Mike <29170001+mike-prentice@users.noreply.github.com>
2022-03-23 20:49:46 -0400

    fly/flyTest

--- src/test/java/com/zipcodewilmington/froilansfarm/Interfaces/FlyTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/Interfaces/FlyTest.java
@@ -2,6 +2,20 @@ package com.zipcodewilmington.froilansfarm.Interfaces;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.zipcodewilmington.froilansfarm.Vehicles.CropDuster;
+
+import org.junit.Test;
+
 class FlyTest {
+
+@Test
+
+public void flyTest(){
+    CropDuster cropduster = new CropDuster();
+    boolean expected = true;
+    boolean actual = cropduster.fly();
+    assertTrue(actual);
+}
+
 
 }
--- src/test/java/com/zipcodewilmington/froilansfarm/Vehicles/VehicleTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/Vehicles/VehicleTest.java
@@ -2,6 +2,13 @@ package com.zipcodewilmington.froilansfarm.Vehicles;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.Test;
+
 class VehicleTest {
+
+    @Test
+    public String makeNoiseTest(){
+        return "";
+    }
 
 }
