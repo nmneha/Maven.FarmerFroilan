aecf34167325211605fa443a9d9990601232cc7b
nusera <nusera.neha@gmail.com>
2022-03-24 14:43:31 -0400

    added egg class

--- src/main/java/com/zipcodewilmington/froilansfarm/Crops/CornStalk.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Crops/CornStalk.java
@@ -1,4 +1,10 @@
 package com.zipcodewilmington.froilansfarm.Crops;
 
+import com.zipcodewilmington.froilansfarm.Interfaces.Edible;
+
 public class CornStalk extends Crop{
+
+    public Edible yield() {
+        return null;
+    }
 }
--- src/main/java/com/zipcodewilmington/froilansfarm/Crops/TomatoPlant.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Crops/TomatoPlant.java
@@ -1,9 +1,11 @@
 package com.zipcodewilmington.froilansfarm.Crops;
 
+import com.zipcodewilmington.froilansfarm.Interfaces.Edible;
+
 public class TomatoPlant extends Crop {
 
-    public int yield() {
-        return 0;
+    public Edible yield() {
+        return null;
     }
 
 
