e37dbab533f83c484b478f679b165536de546ea9
nusera <nusera.neha@gmail.com>
2022-03-24 14:40:04 -0400

    changes to cropRow and field

--- src/main/java/com/zipcodewilmington/froilansfarm/Crops/Crop.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Crops/Crop.java
@@ -4,14 +4,4 @@ import com.zipcodewilmington.froilansfarm.Interfaces.Produce;
 
 public  abstract class Crop implements Produce {
 
-    public int yield(){ return 0;
-    }
-
-    public boolean hasBeenHarvested() {
-        return true;
-    }
-
-    public boolean hasBeenFertilized() {
-        return true;
-    }
 }
--- src/main/java/com/zipcodewilmington/froilansfarm/Crops/TomatoPlant.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Crops/TomatoPlant.java
@@ -2,6 +2,9 @@ package com.zipcodewilmington.froilansfarm.Crops;
 
 public class TomatoPlant extends Crop {
 
+    public int yield() {
+        return 0;
+    }
 
 
 }
