75c88b13e9cda2c80de9f451201ab3fc3c3e7e4b
Ethan <ethan@zipcoders-MacBook-Pro-4.local>
2022-03-27 12:22:07 -0400

    Added a few tests

---
+++ src/test/java/com/zipcodewilmington/froilansfarm/Interfaces/BotanistTest.java
@@ -0,0 +1,46 @@
+package com.zipcodewilmington.froilansfarm.Interfaces;
+
+import com.zipcodewilmington.froilansfarm.Animals.Farmer;
+import com.zipcodewilmington.froilansfarm.Crops.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BotanistTest {
+
+    @Test
+    void plantRowTest() {
+        Farmer<Crop> bob = new Farmer();
+        TomatoPlant tomato = new TomatoPlant();
+        CornStalk corn = new CornStalk();
+        CropRow row = new CropRow();
+        CropRow row2 = new CropRow();
+        bob.plantRow(row,tomato);
+        bob.plantRow(row2,corn);
+
+
+        assertTrue(row.getCrop(0) instanceof Crop);
+        //assertTrue(row.getCrop(0) instanceof CornStalk);
+    }
+
+//    @Test
+//    void plantFieldTest() {
+//        Farmer<Crop> bob = new Farmer();
+//        TomatoPlant tomato = new TomatoPlant();
+//        CornStalk corn = new CornStalk();
+//        Field field = new Field();
+//        field.add(new CropRow());
+//        field.add(new CropRow());
+//        bob.plantField(field,tomato,corn);
+//        ArrayList<CropRow> expected = new ArrayList<>();
+//        CropRow row = new CropRow();
+//        CropRow row2 = new CropRow();
+//        expected.add(bob.plantRow(row,tomato));
+//        expected.add(bob.plantRow(row2,corn));
+//
+//        assertEquals(expected.get(0),field.getCropRow(0));
+//
+//    }
+}
